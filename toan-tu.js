0d6cadd67033f73fcba9747bf5f0c0880fe947c8
chrisph124 <chris.pham124@gmail.com>
2016-12-26 23:58:44 +0700

    Toán tử gán thông dụng trong JavaScript

--- toan-tu.js
+++ toan-tu.js
@@ -1,3 +1,26 @@
 /**
  * Created by chris on 26/12/2016.
  */
+//Danh sách các toán tử gán thường dùng trong JavaScript
+
+//Toán tử ( = )
+var a = 10;
+var b = a;
+console.log(b);
+
+//Toán tử ( += )
+var c = 10;
+var d = 12;
+// c += d --> c = c + d nên lúc này c = 10 + 12 = 22;
+console.log(c += d);
+
+//Toán tử ( -= )
+//Tương tự như =+ và lúc này e = 15 - 10 = 5
+var e = 15;
+var f = 10;
+console.log(e -= f);
+
+//Toán tử ( *= ), ( /= ), ( %= )
+//Tương tự như += hay -=
+
+
